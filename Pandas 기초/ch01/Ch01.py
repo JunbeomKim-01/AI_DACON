06b35d3b76dd42e5830c2f9f14e4b99a4016dad6
junbeom <72601028+JunbeomKim-01@users.noreply.github.com>
2021-07-18 23:56:08 +0900

    ch01

--- Pandas 기초/ch01/Ch01.py
+++ Pandas 기초/ch01/Ch01.py
@@ -1,6 +1,14 @@
-
+import pandas as pd
 
 #파일과 데이터 프레임
 
-if __name__ == '__main__':
-    print('dsa')
+data=pd.read_csv("/content/drive/MyDrive/pandas/대한민국_주요_도시_인구수.csv")
+data
+
+data.shape
+
+data.info()#데이터 프레임의 요약 정보
+
+data['도시']#특정 칼럼
+
+data.도시
